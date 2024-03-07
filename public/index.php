01decd3d9f1cbc5c7021561e446b3e14b30ec805
Carlos Teca Avelino <caasciiafricanopc.dev@gmail.com>
2024-03-07 01:11:07 +0100

    template montado

--- public/index.php
+++ public/index.php
@@ -16,10 +16,10 @@
 <body>
 
     <header>
-
+        <!-- Barra de navegação mobile e tablete -->
         <nav style="background-color: #5e2129;">
             <div class="container nav-wrapper">
-                <a href="#" class="brand-logo">Logo</a>
+                <a href="#" class="brand-logo">Garrafeira</a>
                 <ul id="nav-mobile" class="right hide-on-med-and-down">
                     <li>
                         <form action="" method="GET">
@@ -32,63 +32,219 @@
                     <li><a href="sass.html"><i class="material-icons" style="color:#ffd700;">add_shopping_cart</i></a></li>
                     <li>|</li>
                     <li><a href="sass.html">Entrar</a></li>
+                    <li><a class="btn" href="sass.html" style="border-radius: 20px; background-color: #c8a2c8;">Cadastrar</a></li>
                 </ul>
             </div>
         </nav>
     </header>
 
-    <main class="container">
+    <main class="">
         <div class="row">
             <!-- Barra lateral para pesquisa filtarada -->
-            <div class="col s12 m4">
-                This div is 12-columns wide on all screen sizes>
+            <div class="container col s12 m4">
+                <div class="" style="width:100%; padding: 5%;">
+
+                    <h4>Filtros</h4>
+                    <hr>
+
+                    <form class="container" action="" method="get">
+                        <h6>Produtos</h6>
+                        <p>
+                            <label>
+                                <input type="checkbox" name="txt_filtro_por_vinho" class="filled-in" checked="" />
+                                <span>Vinhos</span>
+                            </label>
+                        </p>
+                        <p>
+                            <label>
+                                <input class="filled-in" name="txt_filtro_por_cerveja" type="checkbox" checked="" />
+                                <span>Cervejas</span>
+                            </label>
+                        </p>
+                        <p>
+                            <label>
+                                <input class="filled-in" name="txt_filtro_whisky" type="checkbox" checked="" />
+                                <span>Whisky</span>
+                            </label>
+                        </p>
+                        <p>
+                            <label>
+                                <input class="filled-in" name="txt_filtro_por_gasosa" type="checkbox" checked="" />
+                                <span>Gasosas</span>
+                            </label>
+                        </p>
+                    </form>
+                </div>
+
             </div>
             <!-- Area das Garrafas -->
             <div class="col s12 m8">
                 <div class="row">
-                    <div class="col s12 m3">
-                        adasd
+                    <!-- Cartão -->
+                    <div class="container col s12 m4">
+                        <div class="card small">
+                            <img class="responsive-img" src="assets/uploads/whisky.png" style="width: 100%;">
+
+                            <div class="card-action">
+                                <div class="row">
+                                    <div class="col s3">
+                                        <a href="">
+                                            <i class="material-icons" style="color:#111;">remove_red_eye</i>
+                                        </a>
+                                    </div>
+                                    <div class="col s3">
+                                        <a href="">
+                                            <i class="material-icons" style="color:#111;">airplay</i>
+                                        </a>
+                                    </div>
+                                    <div class="col s3">
+                                        <a href="">
+                                            <i class="material-icons" style="color:#111;">add_shopping_cart</i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="col s12 m3">
-                        adasd
+                    <!-- Cartão -->
+                    <div class="container col s12 m4">
+                        <div class="card small">
+                            <img class="responsive-img" src="assets/uploads/whisky.png" style="width: 100%;">
+
+                            <div class="card-action">
+                                <div class="row">
+                                    <div class="col s3">
+                                        <a href="">
+                                            <i class="material-icons" style="color:#111;">remove_red_eye</i>
+                                        </a>
+                                    </div>
+                                    <div class="col s3">
+                                        <a href="">
+                                            <i class="material-icons" style="color:#111;">airplay</i>
+                                        </a>
+                                    </div>
+                                    <div class="col s3">
+                                        <a href="">
+                                            <i class="material-icons" style="color:#111;">add_shopping_cart</i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="col s12 m3">
-                        adasd
+                    <!-- Cartão -->
+                    <div class="container col s12 m4">
+                        <div class="card small">
+                            <img class="responsive-img" src="assets/uploads/whisky.png" style="width: 100%;">
+
+                            <div class="card-action">
+                                <div class="row">
+                                    <div class="col s3">
+                                        <a href="">
+                                            <i class="material-icons" style="color:#111;">remove_red_eye</i>
+                                        </a>
+                                    </div>
+                                    <div class="col s3">
+                                        <a href="">
+                                            <i class="material-icons" style="color:#111;">airplay</i>
+                                        </a>
+                                    </div>
+                                    <div class="col s3">
+                                        <a href="">
+                                            <i class="material-icons" style="color:#111;">add_shopping_cart</i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="col s12 m3">
-                        adasd
+
+                    <!-- Cartão -->
+                    <div class="container col s12 m4">
+                        <div class="card small">
+                            <img class="responsive-img" src="assets/uploads/vinho.png" style="width: 100%;">
+
+                            <div class="card-action">
+                                <div class="row">
+                                    <div class="col s3">
+                                        <a href="">
+                                            <i class="material-icons" style="color:#111;">remove_red_eye</i>
+                                        </a>
+                                    </div>
+                                    <div class="col s3">
+                                        <a href="">
+                                            <i class="material-icons" style="color:#111;">airplay</i>
+                                        </a>
+                                    </div>
+                                    <div class="col s3">
+                                        <a href="">
+                                            <i class="material-icons" style="color:#111;">add_shopping_cart</i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Cartão -->
+                    <div class="container col s12 m4">
+                        <div class="card small">
+                            <img class="responsive-img" src="assets/uploads/vinho.png" style="width: 100%;">
+
+                            <div class="card-action">
+                                <div class="row">
+                                    <div class="col s3">
+                                        <a href="">
+                                            <i class="material-icons" style="color:#111;">remove_red_eye</i>
+                                        </a>
+                                    </div>
+                                    <div class="col s3">
+                                        <a href="">
+                                            <i class="material-icons" style="color:#111;">airplay</i>
+                                        </a>
+                                    </div>
+                                    <div class="col s3">
+                                        <a href="">
+                                            <i class="material-icons" style="color:#111;">add_shopping_cart</i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Cartão -->
+                    <div class="container col s12 m4">
+                        <div class="card small">
+                            <img class="responsive-img" src="assets/uploads/vinho.png" style="width: 100%;">
+
+                            <div class="card-action">
+                                <div class="row">
+                                    <div class="col s3">
+                                        <a href="">
+                                            <i class="material-icons" style="color:#111;">remove_red_eye</i>
+                                        </a>
+                                    </div>
+                                    <div class="col s3">
+                                        <a href="">
+                                            <i class="material-icons" style="color:#111;">airplay</i>
+                                        </a>
+                                    </div>
+                                    <div class="col s3">
+                                        <a href="">
+                                            <i class="material-icons" style="color:#111;">add_shopping_cart</i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col s12 m3">
-                        adasd
-                    </div>
-                    <div class="col s12 m3">
-                        adasd
-                    </div>
-                    <div class="col s12 m3">
-                        adasd
-                    </div>
-                    <div class="col s12 m3">
-                        adasd
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col s12 m3">
-                        adasd
-                    </div>
-                    <div class="col s12 m3">
-                        adasd
-                    </div>
-                    <div class="col s12 m3">
-                        adasd
-                    </div>
-                    <div class="col s12 m3">
-                        adasd
-                    </div>
-                </div>
+
+                <!-- Fim da row -->
             </div>
-            
+
+
         </div>
     </main>
 
